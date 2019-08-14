328b031edf935105fe63ce256cbf8648a5f310a8
podkmax <podkmax@indevstudio.com>
2019-08-14 12:08:41 +0300

    some improvements

---
+++ src/main/java/com/indev/fsklider/graph/nodes/properties/EndProps.java
@@ -0,0 +1,14 @@
+package com.indev.fsklider.graph.nodes.properties;
+
+public class EndProps {
+    private String synthText;
+
+    public String getSynthText() {
+        return synthText;
+    }
+
+    public void setSynthText(String synthText) {
+        this.synthText = synthText;
+    }
+
+}
