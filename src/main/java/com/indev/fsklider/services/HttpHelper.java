b37d947a05e892bc2b0cab35982e6799fa8abcbd
m.kondratev <m.kondratev@indevstudio.com>
2019-10-11 12:21:13 +0300

    test redmine Api

--- src/main/java/com/indev/fsklider/services/HttpHelper.java
+++ src/main/java/com/indev/fsklider/services/HttpHelper.java
@@ -3,22 +3,42 @@ package com.indev.fsklider.services;
 import com.indev.fsklider.beans.ResponseEvent;
 import com.indev.fsklider.graph.context.Context;
 import com.indev.fsklider.graph.results.DialogResult;
+import com.taskadapter.redmineapi.bean.Issue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class HttpHelper {
-    public void doPost(String url, Context context){
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public void doPost(String url, Context context) {
         RestTemplate restTemplate = new RestTemplate();
         DialogResult req = context.getResult();
         HttpEntity<DialogResult> request = new HttpEntity<>(req);
         DialogResult dialogResult = restTemplate.postForObject(url, request, DialogResult.class);
     }
 
-    public void doPost (ResponseEvent event){
+    public void doPost(ResponseEvent event) {
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<ResponseEvent> request = new HttpEntity<>(event);
         ResponseEvent dialogResult = restTemplate.postForObject("http://192.168.1.74:8080/event", request, ResponseEvent.class);
+    }
+
+    public <T> T getTest(String url, Class<T> clazz) {
+        return restTemplate.getForObject("http://redmine.indev:3000/", clazz);
+    }
+
+    public <T> T postTest(String url, String data, Class<T> clazz) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Basic cGhvbmVVc2VyOjEyMzQ1Njc4");
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> request = new HttpEntity<>(data, headers);
+        return restTemplate.postForObject(url, request, clazz);
     }
 }
